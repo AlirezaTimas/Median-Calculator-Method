efecc737bd539ce09ecb02dd264a64aeba329eec
Markazi.co <Markazi.co@LAPTOP-VA2L6RTV>
2023-03-26 14:44:43 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Miane_Calculator_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Miane mohasebegar = new Miane();
+            mohasebegar.MianeMohaseb();
+
+
+        }
+    }
+    public class Miane
+    {
+        public void MianeMohaseb()
+        {
+            int[] Araye = new int[10];
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.WriteLine("Adad mored nazar ra vared konid");
+                Araye[i] = int.Parse(Console.ReadLine());
+            }
+            int Temp = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (Araye[j] > Araye[j + 1])
+                    {
+                        Temp = Araye[j];
+                        Araye[j] = Araye[j + 1];
+                        Araye[j + 1] = Temp;
+                    }
+
+
+                }
+            }
+            int sum = (Araye[5] + Araye[6]) / 2;
+            Console.WriteLine(sum);
+        }
+    }
+}
